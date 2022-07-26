738addc83edfbfb960d823b3bd6f1bbededa4efa
devhiko <aydin96tr@gmail.com>
2022-07-26 22:07:45 +0300

    edited

--- components/kiraci/KiraciListe.tsx
+++ components/kiraci/KiraciListe.tsx
@@ -17,50 +17,30 @@ type KiraciListeProps = {
 }
 
 export const KiraciListe = ({ kiracilar }: KiraciListeProps) => {
-  // todo: hiç kiraci yok sayfası ekle
-  // if (true) return (
-  //   <div
-  //     style={{
-  //       borderRadius: '1rem',
-  //       height: '500px',
-  //       border: '1px solid #ccc',
-  //       margin: '1rem',
-  //       position: 'relative'
-  //     }}
-  //   >
+  console.log(kiracilar.length)
+  // if (!kiracilar.length || kiracilar.length === 0) return (
+  //   <Container className={styles.kiraciyok} >
   //     <div className={styles.center}>
-  //       <div style={{ margin: '1rem auto', height: '200px', textAlign: 'center', width: '50%', background: 'gray' }}>
-  //         <img src="/assets/ev.png" width={200} height={200}></img>
+  //       <div className={styles['kiraciyok-image']}>
+  //         <img src="/assets/ev.png" width={100} height={100}></img>
   //       </div>
-  //       <p style={{ textAlign: 'center' }}>
-  //         Hiç kiraci yok, Eklediğinizde burada görünür
-  //       </p>
+  //       <p style={{ textAlign: 'center' }}>Hiç kiracı yok, Eklediğinizde burada görünür</p>
   //     </div>
-  //   </div>
+  //   </Container>
   // )
 
   // todo: kiracilar yükleniyor sayfası ekle
-  // if (true === true) return (
-  //   <div
-  //     style={{
-  //       borderRadius: '1rem',
-  //       height: '500px',
-  //       border: '1px solid #ccc',
-  //       margin: '1rem',
-  //       position: 'relative'
-  //     }}
-  //   >
+  // ? if kiracilar.loding return Loading...
+  // if (kiracilar.loading) return (
+  //   <Container className={styles['kiraci-loading']}>
   //     <div className={styles.center}>
-  //       <div>
-  //         <Spinner animation="border" variant="primary" style={{ margin: '0 2rem', width: '100px', height: '100px', borderWidth: '.25rem' }}>
-  //           <span className="visually-hidden">Kiracılar Yükleniyor...</span>
-  //         </Spinner>
-  //         <p style={{ margin: '2rem 1rem' }}>Kiracılar Yükleniyor...</p>
-  //       </div>
+  //       <Spinner animation="border" variant="primary" className={styles.spinner}>
+  //         <span className="visually-hidden">Kiracılar Yükleniyor...</span>
+  //       </Spinner>
+  //       <p style={{ margin: '1rem auto' }}>Kiracılar Yükleniyor...</p>
   //     </div>
-  //   </div>
+  //   </Container>
   // )
-
 
   // kira takip kart component i
   const kiraKart = () => kiracilar.map((kiraci) => <KiraciKart key={kiraci.sys.id} kiraci={kiraci} />)
