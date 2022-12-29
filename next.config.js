1c944aaf4732802798c434bf31b0139a22730702
hikmet <aydin96tr@gmail.com>
2022-12-29 19:00:49 +0300

    e

--- next.config.js
+++ next.config.js
@@ -1,18 +1,19 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 /** @type {import('next').NextConfig} */
-const withPWA = require('next-pwa')
+const withPWA = require("next-pwa")({
+  dest: "public",
+  customWorkerDir: "serviceworker",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+});
 
-module.exports = withPWA({
+const nextConfig = withPWA({
   reactStrictMode: true,
   swcMinify: true,
   env: {
-    C_SPC_ID: '4xsr7uqyeyi2',
-    C_MNG_TOKEN: 'CFPAT-n2ou0ArAoOZaiqPKj7oXLbI_k80-8Hq5t33ZPWp8YmI',
+    C_SPC_ID: "4xsr7uqyeyi2",
+    C_MNG_TOKEN: "CFPAT-n2ou0ArAoOZaiqPKj7oXLbI_k80-8Hq5t33ZPWp8YmI",
   },
-  pwa: {
-    dest: "public",
-    customWorkerDir: 'serviceworker',
-    register: true,
-    skipWaiting: true,
-    disable: process.env.NODE_ENV === 'development'
-  }
-})
+});
+module.exports = nextConfig;
